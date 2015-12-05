d555a442ffc1fe6ee91bcfb79a00bbdbe364836c
Logan Franken <loganfranken@gmail.com>
2015-12-05 00:15:25 -0800

    Implemented basic search for UCSB repos

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-console.log('Hello world!');
+var request = require('request');
+
+var options = {
+  url: 'https://api.github.com/search/repositories?q=ucsb',
+  headers: {
+    'User-Agent': 'github-search'
+  }
+};
+
+// Search Repositories
+var urls = [];
+var pageCount = 1;
+
+function searchRepositories()
+{
+  options.url += ('&page=' + pageCount);
+
+  request(options, function (error, response, body) {
+
+    var results = JSON.parse(body);
+
+    if(!results.items || results.items.length === 0) {
+      outputResults();
+      return;
+    }
+
+    results.items.forEach(function(result, i) {
+      urls.push(result.html_url);
+    });
+
+    pageCount++;
+    searchRepositories();
+
+  });
+}
+
+function outputResults()
+{
+  // Display Results
+  console.log('Total Results: ' + urls.length);
+
+  urls.forEach(function(url, i) {
+    console.log(url);
+  });
+}
+
+searchRepositories();
